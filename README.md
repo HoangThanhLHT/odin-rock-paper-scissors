f578ceb172387c8c766e74e2a4f17b1477c5086d
HoangThanhLHT <191544829+HoangThanhLHT@users.noreply.github.com>
2025-01-21 16:38:26 +0700

    write logic to get computer choice

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # odin-rock-paper-scissors
+
+## Understand Problem
+
+Write a program play rock-paper-scissors with computer
+write a function that randomly returns "rock", "paper", "scissors".
+
+## Pseudocode
+
+
+Create function `getComputerChoice`
+    create variable `randomChoice` return random integer number between 0 and 2
+    return choice 'rock' 'paper' 'scissors' base on `randomChoice`
--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+// test if script.js link with index.html correctly
+// console.log("Hello World!")
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber);
+    let choice;
+    // console.log(choice);
+    if (randomNumber === 0) {
+        choice = 'rock'
+    } else if (randomNumber === 1) {
+        choice = 'paper'
+    } else {
+        choice = 'scissors'
+    }
+    return choice;
+};
+
+// test function getComputerChoice
+console.log(getComputerChoice());
